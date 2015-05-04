116013e598068460fd368eafdb40eb216a1ff972
Fredy Setz <fredy.setz@gmx.ch>
2015-05-05 00:14:25 +0200

    domain implemented

---
+++ plugins/org.scenarioo.example.e4.libs/src/org/scenarioo/example/e4/domain/AbstractDomainEntity.java
@@ -0,0 +1,88 @@
+/* Copyright (c) 2014, scenarioo.org Development Team
+ * All rights reserved.
+ *
+ * See https://github.com/scenarioo?tab=members
+ * for a complete list of contributors to this project.
+ *
+ * Redistribution and use of the Scenarioo Examples in source and binary forms,
+ * with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.scenarioo.example.e4.domain;
+
+
+public abstract class AbstractDomainEntity<T extends AbstractId> {
+
+	private T abstractId;
+
+	public AbstractDomainEntity() {
+	}
+
+	public AbstractDomainEntity(final T abstractId) {
+		this.abstractId = abstractId;
+	}
+
+	public T getIdWrapper() {
+		if (abstractId == null) {
+			return null;
+		}
+		return abstractId;
+	}
+
+	public void generateAndSetId(final IdGenerator counter) {
+		if (abstractId != null) {
+			throw new IllegalStateException("Id already exist!");
+		} else {
+			abstractId = createInstance(counter.next());
+		}
+	}
+
+	protected abstract T createInstance(Long id);
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((abstractId == null) ? 0 : abstractId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		AbstractId other = (AbstractId) obj;
+		return abstractId != null && abstractId.equals(other);
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getName() + " [id=" + (abstractId != null ? abstractId.getId() : "null") + "]";
+	}
+}
---
+++ plugins/org.scenarioo.example.e4.libs/src/org/scenarioo/example/e4/domain/AbstractId.java
@@ -0,0 +1,81 @@
+/* Copyright (c) 2014, scenarioo.org Development Team
+ * All rights reserved.
+ *
+ * See https://github.com/scenarioo?tab=members
+ * for a complete list of contributors to this project.
+ *
+ * Redistribution and use of the Scenarioo Examples in source and binary forms,
+ * with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.scenarioo.example.e4.domain;
+
+
+/**
+ * To make the Id TypeSafe.
+ * 
+ */
+public abstract class AbstractId {
+
+    private final Long id;
+
+    /**
+     * Initialize the IdWrapper.
+     * 
+     * @param id
+     */
+	protected AbstractId(final Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        AbstractId other = (AbstractId) obj;
+        return id != null && id.equals(other.id);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + " [id=" + getId() + "]";
+    }
+
+}
