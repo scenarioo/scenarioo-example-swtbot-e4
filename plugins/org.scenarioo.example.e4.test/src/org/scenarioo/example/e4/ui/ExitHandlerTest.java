722cf3d0f25fb8d06c62a994376dfa4cc261894a
Fredy Setz <fredy.setz@gmx.ch>
2015-06-03 09:30:07 +0200

    integrate SWTBot

---
+++ plugins/org.scenarioo.example.e4.test/src/org/scenarioo/example/e4/ui/ExitHandlerTest.java
@@ -0,0 +1,70 @@
+/* Copyright (c) 2014, scenarioo.org Development Team
+ * All rights reserved.
+ *
+ * See https://github.com/scenarioo?tab=members
+ * for a complete list of contributors to this project.
+ *
+ * Redistribution and use of the Scenarioo Examples in source and binary forms,
+ * with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice, this
+ *   list of conditions and the following disclaimer in the documentation and/or
+ *   other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.scenarioo.example.e4.ui;
+
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotMenu;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SWTBotJunit4ClassRunner.class)
+@Ignore
+public class ExitHandlerTest {
+
+	private static SWTBot bot;
+
+	@BeforeClass
+	public static void beforeClass() throws Exception {
+		// don't use SWTWorkbenchBot here which relies on Platform 3.x
+		bot = new SWTBot();
+	}
+
+	@Test
+	public void executeExit() {
+		// testing on hard-coded String just to have
+		// a simple example
+
+		SWTBotMenu fileMenu = bot.menu("File");
+		Assert.assertNotNull(fileMenu);
+		SWTBotMenu exitMenu = fileMenu.menu("Exit");
+		Assert.assertNotNull(exitMenu);
+		exitMenu.click();
+	}
+
+	@AfterClass
+	public static void sleep() {
+		bot.sleep(2000);
+	}
+}
